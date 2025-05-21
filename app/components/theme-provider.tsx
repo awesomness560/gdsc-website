bd2f9eb9b128dcc06f305ba51cf5c1b31c0fe685
awesomness560 <appsforyou6@gmail.com>
2025-05-21 13:25:57 -0500

    Fixed server side rendering

--- app/components/theme-provider.tsx
+++ app/components/theme-provider.tsx
@@ -26,9 +26,14 @@ export function ThemeProvider({
   storageKey = "vite-ui-theme",
   ...props
 }: ThemeProviderProps) {
-  const [theme, setTheme] = useState<Theme>(
-    () => (localStorage.getItem(storageKey) as Theme) || defaultTheme
-  );
+  const [theme, setTheme] = useState<Theme>(() => {
+    if (typeof window !== "undefined") {
+      const storedTheme = localStorage.getItem(storageKey) as Theme;
+      return storedTheme !== null ? storedTheme : defaultTheme;
+    } else {
+      return defaultTheme;
+    }
+  });
 
   useEffect(() => {
     const root = window.document.documentElement;
@@ -48,7 +53,8 @@ export function ThemeProvider({
   const value = {
     theme,
     setTheme: (theme: Theme) => {
-      localStorage.setItem(storageKey, theme);
+      if (typeof window !== "undefined")
+        window.localStorage.setItem(storageKey, theme);
       setTheme(theme);
     },
   };
